a125bbe5334b1c24176493ce684b41041e3a7739
Carlos <i92cojic@uco.es>
2022-12-17 19:29:27 +0100

    Paprobar

--- src/main/java/servlets/nuevaPistaServlet.java
+++ src/main/java/servlets/nuevaPistaServlet.java
@@ -2,6 +2,7 @@ package servlets;
 
 import java.io.IOException;
 
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import business.Pista_Kart.PistaDTO;
 import data.Pista_Kart.PistaDAO;
 import display.CustomerBean;
+import business.Pista_Kart.Dificultades;
 
 import java.time.*;
 
@@ -19,13 +21,13 @@ import java.time.*;
  * Servlet implementation class nuevaPista
  */
 @WebServlet("/nuevaPista")
-public class nuevaPista extends HttpServlet {
+public class nuevaPistaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public nuevaPista() {
+    public nuevaPistaServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -46,18 +48,17 @@ public class nuevaPista extends HttpServlet {
 		CustomerBean userBean = (CustomerBean) request.getSession().getAttribute("userBean");
 		if (userBean == null || userBean.getEmail() != null) {
 			String nombrePista = request.getParameter("nombrePista");
-			String tipoEstado = request.getParameter("tipoEstado");
-			String dificultad = request.getParameter("dificultad");
-			String maxkarts = request.getParameter("maxkarts");
-
+			String dificultad = request.getParameter("pistaType");
+			String maxkarts = request.getParameter("maxKarts");
+			Dificultades dificultades = Dificultades.valueOf(dificultad);
 			
-			PistaDAO pistaDAO = new PistaDAO();
+
+			PistaDAO pistaDAO = new PistaDAO(userBean.getprop(), userBean.getjdbc(), userBean.getdbuser(), userBean.getdbpass());
 			PistaDTO pista = new PistaDTO();
 			
 			pista.setNombrePista(nombrePista);
-			pista.setTipoEstado(tipoEstado);
-			pista.setDificultad(dificultad);
-			pista.setMaxKarts(maxKarts);
+			pista.setDificultad(dificultades);
+			pista.setMaxKarts(Integer.parseInt(maxkarts));
 			pistaDAO.guardarPista(pista);
 			
 			String msg = "Pista guardada correctamente";
--- src/main/webapp/mvc/view/addPista.jsp
+++ src/main/webapp/mvc/view/addPista.jsp
@@ -1,3 +1,13 @@
+<%@ page import="business.Usuario.UsuarioDTO, data.Usuario.UsuarioDAO"%>
+<jsp:useBean id="userBean" scope="session" class="display.CustomerBean"></jsp:useBean>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%
+	if (userBean.getEmail() == null){
+		response.sendRedirect(request.getContextPath());
+	}
+	else {
+%>
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -7,24 +17,24 @@
             <title>Nueva pista</title>
         </head>
         <body>
+        <%@ include file="../../include/headerAdministrador.jsp" %>
             <div class="container">
                 <h1>Crear pista</h1>
                 <form>
                     <br/>
                     <label for="pistaName">Nombre</label>
                     <input type="text" class="input-form" name="pistaName">
+                    <label for="maxKarts">Numero de karts maximo</label>
+                    <input type="text" class="input-form" name="maxKarts">
                     <br/>
                     <label for="pistaType">Tipo</label>
                     <select id="pistaType" class="select-type" name="pistaType">
-                        <option value="">Infantil</option>
-                        <option value="">Familiar</option>
-                        <option value="">Adultos</option>
-                    </select>
-                    <select id="pistaType" class="select-type" name="pistaType">
-                        <option value="">Reservado</option>
-                        <option value="">Disponible</option>
-                        <option value="">Mantenimiento</option>
+                        <option value="">INFANTIL</option>
+                        <option value="">FAMILIAR</option>
+                        <option value="">ADULTOS</option>
                     </select>
                     <br/>
+                    </div>
+                  </form> 
                 </body>
-                </html>
+		</html>
